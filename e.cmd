1e90d64c0fef535623d3cc7340bb9386e6520c95
Your Name <you@example.com>
2017-02-01 09:20:49 +0800

    Initial fix for timeout issues using the simplest working approach

---
+++ e.cmd
@@ -0,0 +1,4 @@
+set JAVA_HOME=C:\Java\jdk1.8.0_121
+set JAVA_OPTS=-Xmx4096m
+set ACTIVATOR_OPTS=%JAVA_OPTS%
+set PATH=C:\Users\Asus\Downloads\typesafe-activator-1.3.12\activator-dist-1.3.12\bin;%JAVA_HOME%\bin;%PATH%
